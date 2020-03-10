591604a7c2c685cd8f8ec228751ef534bd94d33e
n-d-k <ndkmail@icloud.com>
2020-03-10 01:19:52 -0500

    Added text scaling down for screen height below 800px

--- NdkBootPicker.c
+++ NdkBootPicker.c
@@ -1038,6 +1038,7 @@ InitScreen (
     mIconSpaceSize = 288;
   } else if (mUiScale == 0 && mScreenHeight <= 800) {
     mUiScale = 8;
+    mTextScale = 8;
     mIconPaddingSize = 3;
     mIconSpaceSize = 70;
   } else {
